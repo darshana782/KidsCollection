f63ea5df146b3da74fdef0d1ddf2dfa3dd9bb6aa
Prabhath Darshana <2018is012@stu.ucsc.cmb.ac.lk>
2021-09-22 01:26:42 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,69 @@
+<html>
+
+<head>
+	<title>About us</title>
+	<link rel="stylesheet" href="about_css.css" type="text/css">
+</head>
+
+<body bgcolor="#e6ffff">
+	<div class="container">
+
+				<ul class="link_list_for_others">
+					
+					<li class="nav_li"><a href="contact.php"> CONTACT US </a></li>
+					<li style="float:left" class="nav_li"><a class="active" href="about.php"> ABOUT US </a></li>
+					<li class="nav_li"><a href="services.php"> SERVICES </a></li>
+					<li class="nav_li"><a href="gallery.php?id=100"> GALLERY </a></li>
+					<li class="nav_li"><a href="home.php"> HOME </a></li>
+
+				</ul>
+
+
+
+			<div class="middle">
+				<div class="about">
+					
+					<br><br>
+					<img src="images/logo-babyworld.png" alt="company logo"  align="left" >
+					<img src="images/logo-babyworld.png" alt="company logo"  align="right" >
+
+
+					<h1 align="center">ABOUT US </h1>
+					
+					<div class="center">
+					Was established in 2010, in the city of Colombo. Commenced as Kids Collection (PVT) Ltd .In its early years although the company faced many obstacles the ‘Goodwill’ created among its clients, contributed and motivated it to grow and expand into present status.
+					<br><br>With the opening of its retail outlets and show rooms as “……new name……….”, the company gradually cemented its reputation as a reliable supplier of quality baby care and kids toys products. This tremendous success and growth was achieved due to the far-sighted policies and efficient management techniques employed by the company together with the maintenance of a very high standard of product quality.
+					<br><br>Today with an extensive range of quality Baby Care & Toys ranging from New Born to age up to six years, 'NEWNAME' is positioned as one of the leading suppliers in the trade with a network of outlets spread throughout the country.
+					<br><br>
+					Step over the threshold and you enter a world of joy and wonder. Over the squeaking, bleeping, whizzing and chiming of toys, you hear children laughing excitedly as they goggle at the sheer choice over world of toys and games.
+					<br><br>We want all of our customers, young and old, to be more than happy with the toys they are buying and aim for these toys to fulfil expectations - always.
+					<br><br>With industry trends and demands ever increasing, the company too strives hard to meet this challenge by constantly increasing its variety and product range with quality products from many leading international suppliers.
+					<br><br>
+					<i><b>Today, with over 10 years of experience in bringing smiles and laughter to children, “KIDS COLLECTION” is known and trusted all over the country. We are proud to have created an environment where life at work is enjoyable, fun and rewarding and we welcome passionate, energetic people to be part of our family!</b></i>
+					</div>	
+					
+				</div>
+				
+				
+
+			</div>
+
+		<div class="bottom_fix">
+			<h3>Kids Collection</h3>
+			<p>Copyright ©ucsc. All Rights Reserved.</p>
+			<img src="images/social_icons.png">		
+			<span>
+				<h2 style="color: #ffb936">
+					Our Mission
+				</h2>
+					To bring joy, happiness and inspiration to babies and families around the world by providing them <br> 
+				  with products and services that embody love.
+			</span>				
+		</div>	
+		
+	</div>
+
+
+
+</body>
+</html>
