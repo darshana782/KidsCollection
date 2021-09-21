2abb77a4e3e9dbd266b925e527d8784c56452e7a
Prabhath Darshana <2018is012@stu.ucsc.cmb.ac.lk>
2021-09-22 01:28:13 +0530

    Add files via upload

---
+++ buyer_t&c.php
@@ -0,0 +1,113 @@
+<?php
+	$con=mysqli_connect("localhost","root","","kids_collection") or die('connection failed');
+?>
+
+
+<html>
+
+<head>
+	<title>User | buyer T & C</title>
+
+	<link rel="stylesheet" href="sign_css.css" type="text/css">
+	<link rel="stylesheet" href="TNSNEW.css" type="text/css">
+
+</head>
+
+<body bgcolor="#e6ffff">
+	<div class="container">
+
+				<ul class="link_list_for_others">
+					
+					<li style="float:left" class="nav_li"><a class="active" href="buyer_t&c.php"> Terms & Conditions for Buyers </a></li>
+					<li class="nav_li"><a href="contact.php"> CONTACT US </a></li>
+					<li class="nav_li"><a href="about.php"> ABOUT US </a></li>
+					<li class="nav_li"><a href="services.php"> SERVICES </a></li>
+					<li class="nav_li"><a href="gallery.php?id=100"> GALLERY </a></li>
+					<li class="nav_li"><a href="home.php"> HOME </a></li>
+
+				</ul>
+
+			<div class="middle">
+
+				<img src="images/terms.jpg" class="tc_back_image">
+				
+				<section>
+				<div class="box">
+					
+					
+					<h2 id="topic">Terms and Conditions</h2>
+					
+					We are committed to sourcing, creating and supplying innovative products that possess a diverse range. We are also committed to provide you with products of top quality that actually exceed the high expectations of customers. We are dedicated to providing information, advice and enthusiastic after-sales service to help you maximize your sales effort.
+					
+					<ol>
+						<li class="word">
+							<h4>Damaged or faulty items</h4>
+							We will take reasonable steps to pack the goods properly and to ensure that you receive your order in good condition.<br>
+							If you receive an item which has been damaged in transit, please contact us as soon as possible. We will do our best to provide a replacement as soon as possible.<br><br>
+							In the unlikely event that you receive an item which proves to be faulty we are happy to help. However, we do ask that you notify us of any problem within a reasonable period of time. We reserve the right to test the product before a suitable course of action is decided upon.<br>
+							<br>Checked that the goods are faulty, we may: <br><br>
+								<ul style="list-style-type:square;">
+									<li>Provide you with a full or partial refund; or </li>
+									<li>Replace the goods; or </li>
+									<li>Repair the goods.</li>
+								</ul>
+								
+						</li>
+						<li>
+							<h4>Warranty and Claims</h4>
+							
+							We shall not be liable for any defect in the goods arising from fair wear and tear, wilful damage, accident, negligence by you or any third party, if you use the goods in a way that we or the manufacturers do not recommend, you fail to follow the manufacturer’s and/or our instructions, or any alteration or repair you carry out without our prior written approval.
+						    
+						</li>
+						<li>
+							<h4>Return items</h4>
+							If you change your mind or if you are unhappy with the goods received and wish to return your items, you should return back within 14 working days along with the original receipt.
+							<br>And any refunds will be issued once we have received the goods, in their original packaging and in a re-saleable condition.
+						</li>
+						<li>
+							<h4>Privacy Policy</h4>
+							We respect your privacy and are committed to handling your personal information. we manage your personal information that we collect about you in a responsible way.<br>
+							We will not disclose your personal information without your consent unless disclosure is either necessary to prevent a threat to life or health, authorized or required by law, reasonably necessary to enforce the law or necessary to investigate a suspected unlawful activity.
+						</li>
+						<li>
+							<h4>Changes to our policy</h4>
+							From time to time it may be necessary for us to review and revise this Privacy Policy. We reserve the right to change our Privacy Policy at any time. Amendments or replacements of the Privacy Policy will be posted on the website.
+						</li>
+						<li>
+							<h4>Contact</h4>
+							Where you have an query or issue relating to your goods, please immediately contact us. Please have your invoice number on hand, along with the specifics of your purchased goods.
+						</li>
+					</ol>
+
+
+					</div>
+				</section>
+
+			</div>
+
+		<div class="bottom_fix_tc">
+			<h3>Kids Collection</h3>
+			<p>Copyright ©ucsc. All Rights Reserved.</p>
+			<img src="images/social_icons.png">		
+			<span>
+				<h2 style="color: #ffb936">
+					Our Mission
+				</h2>
+					To bring joy, happiness and inspiration to babies and families around the world by providing them <br> 
+				  with products and services that embody love.
+			</span>			
+		</div>
+
+
+	</div>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
