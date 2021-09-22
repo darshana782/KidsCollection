05d1d162252606c792152e3602f2231e0293c6bf
Prabhath Darshana <2018is012@stu.ucsc.cmb.ac.lk>
2021-09-22 11:55:54 +0530

    Add files via upload

---
+++ supplier_guide.php
@@ -0,0 +1,71 @@
+<html>
+
+<head>
+	<title>login</title>
+
+	<link rel="stylesheet" href="supplier_guide_css.css" type="text/css">
+
+</head>
+
+<body bgcolor="#e6ffff" >
+	<div class="container">
+
+				<ul class="link_list_for_others">
+					
+					<li class="nav_li"><a href="contact.php"> CONTACT US </a></li>
+					<li style="float:left" class="nav_li"><a class="active" href="leave_feedback.php"> New Supplier Guide </a></li>
+					<li class="nav_li"><a href="about.php"> ABOUT US </a></li>
+					<li class="nav_li"><a href="services.php"> SERVICES </a></li>
+					<li class="nav_li"><a href="gallery.php?id=100"> GALLERY </a></li>
+					<li class="nav_li"><a href="home.php"> HOME </a></li>
+				
+
+				</ul>
+				
+				<div class="middle">
+				
+			</div>
+			<section>
+			<div class="box">
+			<h2>New Supplier Guide</h2>
+			<p>"Adding New Suppliers" on your "Online Invoices" Stock Management system can be done through  the<br>
+			following  steps:<br>
+			1.Through the "Services" tab, select the "For Suppliers" option.<br>
+			2.Inside it you can see 3 options as Supplier Request,New Supplier Guide and Terms and Conditions.<br>
+			3.Click the "Supplier Request" button.<br><br>
+			
+			<h2>Add the new supplier's details:</h2>
+			<h3>Complete the "Supplier Details".</h3>
+			
+			1.Enter the supplier's full name in "<b>Name:</b>".<br>
+			2.Add your supplier's Business details: "<b>Name</b>", "<b>Telephones</b>", and "<b>Address</b>".<br>
+			3."Email": Enter your supplier's "Email" into the email address field.<br>
+			4."Notes": Add any notes you want to add about the supplier.<br>
+			5.Click on the "Save" button to save the new Supplier.<br><br>
+			According to above steps you can apply for the supplier.If admin accept your request you can log
+			in to our page and build contact with us and take further details regarding supply.</p>
+			</div>
+			</section>						
+			
+
+		
+	</div>
+
+		<div class="bottom_fix">
+			<h3>Kids Collection</h3>
+			<p>Copyright ©ucsc. All Rights Reserved.</p>
+			<img src="images/social_icons.png">		
+			<span>
+				<h2 style="color: #ffb936">
+					Our Mission
+				</h2>
+					To bring joy, happiness and inspiration to babies and families around the world by providing them <br> 
+				  with products and services that embody love.
+			</span>	
+		</div>
+
+
+
+</body>
+</html>
+				
---
+++ supplier_t&c.php
@@ -0,0 +1,117 @@
+<?php
+	$con=mysqli_connect("localhost","root","","kids_collection") or die('connection failed');
+?>
+
+
+<html>
+
+<head>
+	<title>User | suppliyer T & C</title>
+
+	<link rel="stylesheet" href="TNSNEW.css" type="text/css">
+	<link rel="stylesheet" href="sign_css.css" type="text/css">
+
+</head>
+
+<body bgcolor="#e6ffff">
+	<div class="container">
+
+				<ul class="link_list_for_others">
+					
+					<li style="float:left" class="nav_li"><a class="active" href="supplier_t&c.php"> Terms & Conditions for Suppliers </a></li>
+					<li class="nav_li"><a href="contact.php"> CONTACT US </a></li>
+					<li class="nav_li"><a href="about.php"> ABOUT US </a></li>
+					<li class="nav_li"><a href="services.php"> SERVICES </a></li>
+					<li class="nav_li"><a href="gallery.php?id=100"> GALLERY </a></li>
+					<li class="nav_li"><a href="home.php"> HOME </a></li>
+
+				</ul>
+
+			
+			<div class="middle">
+				<img src="images/terms.jpg" class="tc_back_image">
+
+			<section>
+				<div class="box">
+					
+					
+					<h2 class="topic">Terms and conditions for suppliers</h2>
+					
+					
+					
+					One of our core principles is to operate its business both lawfully and ethically, which includes working with suppliers who are aligned to the same values. We expects our suppliers to operate in accordance with applicable laws, respecting individual’s human rights, and mindful of environmental and safety impacts of products and services.<br>
+					This Supplier Code of Conduct is applicable to all suppliers who provide goods or services to “..NAME OF THE COMPANY…”.
+					<ol>
+						<li>
+							<h4><b>Ethical Business Practice</b></h4>
+					
+							Suppliers are expected to have policies and procedures in place to ensure the prevention of unethical business practices, including human rights abuses, money laundering, fraud, bribery, corruption, and other improper payments or other inducements.
+						</li>
+						<li>
+							<h4><b>Fair Treatment and Equal Opportunity</b></h4>
+							Suppliers shall provide equal employment opportunity to all applicants and employees regardless of race, colour, sex, sexual orientation, gender identity, religion, creed, national or ethnic origin, age, disability, citizenship status, veteran status, marital status etc.
+						</li>
+						<li>
+							<h4><b>Forced and Child Labour</b></h4>
+							Suppliers will not use child, slave, forced or obligatory labour and never engage in, or support human trafficking.
+						</li>	
+						<li>
+							<h4><b>Health and Safety</b></h4>
+							Suppliers will provide a clean, safe and healthy working environment to protect the occupational safety of all employees and shall comply with all applicable laws and regulations regarding working conditions. Suppliers must ensure they have the right measures and appropriate health and safety training to prevent accidents and injury at work.
+						</li>
+						<li>
+							<h4><b> Wages and Hours</b></h4>
+							Suppliers shall ensure that working hours, wages and overtime pay in compliance with all applicable laws with the purpose and intention of the relevant law. Workers shall be paid at least the minimum legal wage or a wage that meets local industry standards.
+						</li>
+						<li>
+							<h4><b> Sustainability and the Environment</b></h4>
+							Suppliers should be compliant with local environmental laws and regulations, including those relating to waste disposal, pollution, discharges and air emissions.Suppliers are expected to reduce the environmental impact of their manufacturing process, products and/or services and their waste emissions, employing best practices for sustainability and the environment.
+						</li>
+						<li>
+							<h4><b>Product Quality</b></h4>
+							Suppliers should comply with required international standards. This requirement means that the toys we sell will have been functionally and laboratory tested, to ensure they comply with the relevant Toy Safety Directives.
+						</li>
+						<li>	
+							<h4><b>Confidentiality and Information Security</b></h4>
+							Suppliers must comply with confidentiality clauses as detailed within agreed contracts to protect sensitive “…NAME OF THE COMPANY….” information. Any information related to BDO must be treated with care and any personal data must be protected in accordance with UK data protection legislation. Suppliers should have robust information security processes and controls to protect data. This should include the training of staff on information security and robust IT controls.
+						</li>
+						<li>	
+							<h4><b>Compliance</b></h4>
+							We reserves the right to investigate any instances of a supplier’s non-compliance of which it becomes aware.<br>
+							<b>Non-compliance may lead to voiding or terminating a supplier contract. <b><br>
+							Actual or suspected violations of this Code should be reported to:
+									<br>“…COMPANY EMAIL….” .
+						</li>
+					</ol>
+					
+					</div>
+				
+				<section>
+			</div>
+
+		<div class="bottom_fix_stc">
+			<h3>Kids Collection</h3>
+			<p>Copyright ©ucsc. All Rights Reserved.</p>
+			<img src="images/social_icons.png">		
+			<span>
+				<h2 style="color: #ffb936">
+					Our Mission
+				</h2>
+					To bring joy, happiness and inspiration to babies and families around the world by providing them <br> 
+				  with products and services that embody love.
+			</span>				
+		</div>
+
+
+	</div>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
