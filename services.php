55beb50fee521e4c82626538cbb6533325d31d8b
Prabhath Darshana <2018is012@stu.ucsc.cmb.ac.lk>
2021-09-22 11:54:21 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,174 @@
+<?php require_once('connection.php'); ?>
+
+<html>
+
+<head>
+	<title>login</title>
+	<link rel="stylesheet" href="ser.css" type="text/css">
+	<link rel="stylesheet" href="home_css.css" type="text/css">
+</head>
+
+<body bgcolor="#e6ffff">
+	<div class="container">
+
+				<ul class="link_list_for_others">
+					
+					<li class="nav_li"><a href="contact.php"> CONTACT US </a></li>
+					<li class="nav_li"><a href="about.php"> ABOUT US </a></li>
+					<li style="float:left" class="nav_li"><a class="active" href="services.php"> SERVICES </a></li>
+					<li class="nav_li"><a href="gallery.php?id=100"> GALLERY </a></li>
+					<li class="nav_li"><a href="home.php"> HOME </a></li>
+
+				</ul>
+
+
+
+			<div class="middle">
+
+				<img src="images/servicespic.jpg" style="position: fixed; width: 100%; z-index: -1; opacity: 0.8">
+			
+			
+			<br />
+			<br />
+			
+			
+			<h2 align="center"> General Services </h2>	
+				<div class="split left1">
+					<div class="centered">
+					
+					
+					
+						<h3> Trust and Safety </h3>
+						<ul>
+						<li> Access programs for safe trading </li>
+						<li> Dispute resolution </li>
+					    <li> ID verify:Have your personal information verified to establish proof of identify and build trust </li>
+						
+						<p> Successful businesses invest in trust and safety teams to protect their users, their brand, and their bottom line from malicious users. ...
+						Risk, fraud, and application security teams protect against attacks on the business. The trust and safety team protects against attacks on the user.
+						
+						<p>  Spend less time worrying about online security and more time growing your business. Our highly trained associates and precision processes 
+						equip brands with the resources to prioritize, analyze, investigate, and resolve problems. With continuously optimized detection settings and 
+						algorithms, the systems grow smarter over time.
+						<br />
+						<br />
+
+						<ul>
+						<li>Prioritize: Automated tools flag risky transactions and interactions based on recency, cost, impact, etc.</li>
+						<li>Analyze: Complex processes assess critical data elements and uncover patterns that signal fraudulent or offensive behavior.</li>
+						<li>Investigate: Trained experts scrutinize anomalies and define required actions.</li>
+						<li>Resolve: Teams take necessary actions to resolve the issue, and machine learning allows automated systems to identify future occurrences.</li>
+						</ul>
+					</p>
+
+						</ul>
+						
+						
+					</div>	
+				</div>
+					
+					<div class="split left2">
+						<div class="centered">
+					
+						<h3> Payment </h3>
+						<ul>
+						<li> Find out how to send and receive online payments using a bank account or credit card  </li>
+						<li> Protect your high-value transactions </li>
+					    
+						<p>An e-payment system is a way of making transactions or paying for goods and services through an electronic medium, without the use of checks or cash. 
+						It is also called an electronic payment system or online payment system.</p>
+						<p> One of the most popular online payment methods of our company  are credit and debit cards. Besides them, there are also alternative payment methods, such as bank transfers,
+						electronic wallets, smart cards or bitcoin wallet <br />(bitcoin is the most popular cryptocurrency).</p>
+						
+						</ul>
+						
+						
+					</div>	
+					</div>
+					
+					<div class="split right3">
+						<div class="centered">
+					
+						<h3> Member reputation  </h3>
+						<ul>
+						<li> Feedback forum </li>
+						<li> About me page express yourself through your own personal page</li>
+					    <p>Using social media monitoring tool you can analyze your brand reputation. Get instant access to all public mentions on the internet relevant to our
+						brand and products.Engage and manage an online community. People trust recommendations of others. Building a community is a great way to spread the word about our 
+						company and our products.
+						​</p>
+						
+						</ul>
+						
+						
+					</div>	
+					</div>
+					
+					<div class="split right4">
+						<div class="centered">
+					
+						<h3> Protection  </h3>
+						<ul>
+						<li> Buyer protection </li>
+						    <li>Issued a refund from our company directly for the full purchase price and shipping amount</li>
+							<li>Required to return the unsatisfactory item to the seller</li>
+							<li>Absolved of any further connection with the transaction or obligation to the seller</li>
+
+					<p>In rare cases, the buyer may not be required to return the item to the seller but will still receive a refund.
+						After a decision in a buyer's favor, eBay takes responsibility for recovering funds from the seller, because the buyer has already been reimbursed.</p>
+					    
+						
+						</ul>
+						
+						
+					</div>	
+					</div>
+					
+					<h2 align="center"> Selling Services </h2>	
+					<div class="split left1">
+						<div class="centered">
+					
+						
+						<ul>
+						<h3> Protection and Authentication </h3>
+						
+							<p>Seller protection</p>
+						
+						<h3> Listing Solution </h3>
+						
+							<p>  Sell your item   </p>
+						
+					    <h3> Promote your listings </h3>
+						
+							<p>  Seller Central : get the latest and most comprehensive information about selling  </p>
+						
+						<h3> Warranty Program </h3>
+						
+							<p> Give your buyers assurance and peace of mind by offering them a warranty   </p>
+						
+						
+						</ul>
+						
+						
+					</div>	
+					</div>
+
+		<div class="bottom_fix_ser">
+			<h3>Kids Collection</h3>
+			<p>Copyright ©ucsc. All Rights Reserved.</p>
+			<img src="images/social_icons.png">		
+			<span>
+				<h2 style="color: #ffb936">
+					Our Mission
+				</h2>
+					To bring joy, happiness and inspiration to babies and families around the world by providing them <br> 
+				  with products and services that embody love.
+			</span>				
+		</div>	
+		
+	</div>
+
+
+
+</body>
+</html>
